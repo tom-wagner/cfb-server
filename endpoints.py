3151e6ef4433d67f2440f81e7fc431461f8bd645
Tom Wagner <tom.wagner@xaxis.com>
2019-10-16 11:54:29 -0400

    week seven simulation

--- endpoints.py
+++ endpoints.py
@@ -2,10 +2,10 @@ from flask import Flask, json, request
 from flask_cors import CORS
 
 from constants.conferences import CONFERENCES
-from constants.simulation_results.oct_eleventh import oct_eleventh
+from constants.simulation_results.oct_sixteenth import oct_sixteenth
 from constants.teams import TEAMS
 from external_apis.cf_data import CFData
-from ratings.inputs.data.team_ratings.week_six import TEAM_RATINGS as TR_WEEK_SIX
+from ratings.inputs.data.team_ratings.week_seven import TEAM_RATINGS as TR_WEEK_SEVEN
 
 # FOR RUNNING REAL-TIME
 # from simulate.simulate_regular_season import SimulateRegularSeason
@@ -26,7 +26,7 @@ def get_slash():
 
 @app.route("/team_ratings", methods=['GET'])
 def team_ratings():
-    return json.jsonify(TR_WEEK_SIX)
+    return json.jsonify(TR_WEEK_SEVEN)
 
 
 @app.route("/teams", methods=['GET'])
@@ -34,8 +34,8 @@ def teams():
     try:
         adj_teams_object = {
             team: dict(
-                power_rtgs=TR_WEEK_SIX[team],
-                avg_power_rtg=round(sum([rtg for rtg in TR_WEEK_SIX[team].values()]) / len(TR_WEEK_SIX[team]), 1),
+                power_rtgs=TR_WEEK_SEVEN[team],
+                avg_power_rtg=round(sum([rtg for rtg in TR_WEEK_SEVEN[team].values()]) / len(TR_WEEK_SEVEN[team]), 1),
                 **team_obj,
             )
             for team, team_obj in TEAMS.items()
@@ -61,7 +61,7 @@ def conferences():
 
 @app.route("/simulate", methods=["GET"])
 def simulate():
-    return json.jsonify(oct_eleventh)
+    return json.jsonify(oct_sixteenth)
 
 
 # TO ACTUALLY RUN ON POST
--- ratings/scripts/copy_csvs.py
+++ ratings/scripts/copy_csvs.py
@@ -1,3 +1,4 @@
 # TODO: Turn this into shell script
 
+# cd cd ratings/inputs/data && mkdir week_seven
 # cp -a week_three/. week_four/
