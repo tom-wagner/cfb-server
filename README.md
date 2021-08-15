5616b251d03ae9bac790b3a7a0398141b7fde08d
Tom Wagner <tom-wagner@users.noreply.github.com>
2021-08-15 12:46:57 -0400

    August SP+ update

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # cfb-server
+
+
+### To update:
+* Update team rating CSV's
+* Run update_team_ratings.py
+* Save output in team_ratings folder
+* Run simulate_regular_season.py pointing at updated team ratings file
+* Save output in simulation_results folder
+* Update endpoints.py to point at updated team ratings and updated simulation
+
+
+### To run server
+```
+python endpoints.py
+```
+
--- ratings/inputs/data/team_ratings/this_year/preseason.py
+++ ratings/inputs/data/team_ratings/this_year/preseason.py
@@ -1,392 +1,392 @@
 TEAM_RATINGS = {
    "Air Force":{
-      "SP_PLUS":-6.3
+      "SP_PLUS":-3.1
    },
    "Akron":{
-      "SP_PLUS":-18.7
+      "SP_PLUS":-17.9
    },
    "Alabama":{
-      "SP_PLUS":30.7
+      "SP_PLUS":31.7
    },
    "Appalachian State":{
-      "SP_PLUS":11.2
+      "SP_PLUS":10.8
    },
    "Arizona":{
-      "SP_PLUS":-5.7
+      "SP_PLUS":-4.9
    },
    "Arizona State":{
-      "SP_PLUS":18.0
+      "SP_PLUS":16.3
    },
    "Arkansas":{
-      "SP_PLUS":8.8
+      "SP_PLUS":7.7
    },
    "Arkansas State":{
-      "SP_PLUS":-8.6
+      "SP_PLUS":-5.6
    },
    "Army":{
-      "SP_PLUS":-0.6
+      "SP_PLUS":-1.3
    },
    "Auburn":{
-      "SP_PLUS":14.2
+      "SP_PLUS":15.7
    },
    "Ball State":{
-      "SP_PLUS":-0.4
+      "SP_PLUS":-1.3
    },
    "Baylor":{
-      "SP_PLUS":5.8
+      "SP_PLUS":7.0
    },
    "Boise State":{
-      "SP_PLUS":9.3
+      "SP_PLUS":8.9
    },
    "Boston College":{
-      "SP_PLUS":2.9
+      "SP_PLUS":4.6
    },
    "Bowling Green":{
-      "SP_PLUS":-20.9
+      "SP_PLUS":-20.1
    },
    "Buffalo":{
-      "SP_PLUS":-2.0
+      "SP_PLUS":-1.5
    },
    "BYU":{
-      "SP_PLUS":5.7
+      "SP_PLUS":12.9
    },
    "California":{
-      "SP_PLUS":5.2
+      "SP_PLUS":6.0
    },
    "Central Michigan":{
-      "SP_PLUS":-2.4
+      "SP_PLUS":-2.0
    },
    "Charlotte":{
-      "SP_PLUS":-15.2
+      "SP_PLUS":-12.5
    },
    "Cincinnati":{
-      "SP_PLUS":20.0
+      "SP_PLUS":17.0
    },
    "Clemson":{
       "SP_PLUS":28.6
    },
    "Coastal Carolina":{
-      "SP_PLUS":10.4
+      "SP_PLUS":10.6
    },
    "Colorado":{
-      "SP_PLUS":6.0
+      "SP_PLUS":2.3
    },
    "Colorado State":{
-      "SP_PLUS":-7.2
+      "SP_PLUS":-3.1
    },
    "Connecticut":{
-      "SP_PLUS":-22.3
+      "SP_PLUS":-23.1
    },
    "Duke":{
-      "SP_PLUS":-9.3
+      "SP_PLUS":-8.3
    },
    "East Carolina":{
-      "SP_PLUS":-5.6
+      "SP_PLUS":-3.0
    },
    "Eastern Michigan":{
-      "SP_PLUS":-11.2
+      "SP_PLUS":-5.8
    },
    "Florida":{
-      "SP_PLUS":21.2
+      "SP_PLUS":20.7
    },
    "Florida Atlantic":{
-      "SP_PLUS":-8.0
+      "SP_PLUS":1.1
    },
    "Florida International":{
-      "SP_PLUS":-14.0
+      "SP_PLUS":-9.0
    },
    "Florida State":{
-      "SP_PLUS":4.4
+      "SP_PLUS":6.7
    },
    "Fresno State":{
-      "SP_PLUS":-2.1
+      "SP_PLUS":-0.5
    },
    "Georgia":{
-      "SP_PLUS":24.1
+      "SP_PLUS":25.4
    },
    "Georgia Southern":{
-      "SP_PLUS":-4.7
+      "SP_PLUS":-2.5
    },
    "Georgia State":{
-      "SP_PLUS":-1.9
+      "SP_PLUS":0.5
    },
    "Georgia Tech":{
-      "SP_PLUS":6.3
+      "SP_PLUS":4.0
    },
    "Hawai'i":{
-      "SP_PLUS":-10.2
+      "SP_PLUS":-5.3
    },
    "Houston":{
-      "SP_PLUS":1.4
+      "SP_PLUS":3.7
    },
    "Illinois":{
-      "SP_PLUS":-2.6
+      "SP_PLUS":-0.2
    },
    "Indiana":{
-      "SP_PLUS":14.4
+      "SP_PLUS":13.8
    },
    "Iowa":{
-      "SP_PLUS":19.7
+      "SP_PLUS":18.4
    },
    "Iowa State":{
-      "SP_PLUS":23.1
+      "SP_PLUS":22.0
    },
    "Kansas":{
-      "SP_PLUS":-13.9
-   },
-   "Kansas State":{
-      "SP_PLUS":2.6
-   },
-   "Kent State":{
       "SP_PLUS":-13.2
    },
+   "Kansas State":{
+      "SP_PLUS":4.0
+   },
+   "Kent State":{
+      "SP_PLUS":-6.8
+   },
    "Kentucky":{
-      "SP_PLUS":4.8
+      "SP_PLUS":5.1
    },
    "Louisiana":{
-      "SP_PLUS":15.3
+      "SP_PLUS":13.0
    },
    "Louisiana Monroe":{
-      "SP_PLUS":-21.6
+      "SP_PLUS":-17.0
    },
    "Louisiana Tech":{
-      "SP_PLUS":-15.4
+      "SP_PLUS":-11.2
    },
    "Louisville":{
-      "SP_PLUS":7.6
+      "SP_PLUS":8.2
    },
    "LSU":{
       "SP_PLUS":14.6
    },
    "Marshall":{
-      "SP_PLUS":-3.3
+      "SP_PLUS":0.1
    },
    "Maryland":{
-      "SP_PLUS":12.3
+      "SP_PLUS":9.2
    },
    "Memphis":{
-      "SP_PLUS":5.2
+      "SP_PLUS":7.7
    },
    "Miami":{
-      "SP_PLUS":22.7
+      "SP_PLUS":21.8
    },
    "Miami (OH)":{
-      "SP_PLUS":-13.2
+      "SP_PLUS":-5.9
    },
    "Michigan":{
-      "SP_PLUS":15.3
+      "SP_PLUS":18.3
    },
    "Michigan State":{
-      "SP_PLUS":4.6
+      "SP_PLUS":6.6
    },
    "Middle Tennessee":{
-      "SP_PLUS":-14.1
+      "SP_PLUS":-9.5
    },
    "Minnesota":{
-      "SP_PLUS":13.6
+      "SP_PLUS":15.5
    },
    "Mississippi State":{
       "SP_PLUS":8.1
    },
    "Missouri":{
-      "SP_PLUS":4.7
+      "SP_PLUS":5.0
    },
    "Navy":{
-      "SP_PLUS":-9.2
+      "SP_PLUS":-9.6
    },
    "NC State":{
-      "SP_PLUS":8.1
+      "SP_PLUS":7.8
    },
    "Nebraska":{
-      "SP_PLUS":14.0
+      "SP_PLUS":12.4
    },
    "Nevada":{
-      "SP_PLUS":-1.1
+      "SP_PLUS":2.6
    },
    "New Mexico":{
-      "SP_PLUS":-20.0
+      "SP_PLUS":-13.6
    },
    "North Carolina":{
-      "SP_PLUS":21.7
+      "SP_PLUS":21.0
    },
    "Northern Illinois":{
-      "SP_PLUS":-13.7
+      "SP_PLUS":-12.0
    },
    "North Texas":{
-      "SP_PLUS":-10.5
+      "SP_PLUS":-8.8
    },
    "Northwestern":{
-      "SP_PLUS":-1.1
+      "SP_PLUS":1.0
    },
    "Notre Dame":{
-      "SP_PLUS":14.8
+      "SP_PLUS":17.1
    },
    "Ohio":{
-      "SP_PLUS":-5.2
+      "SP_PLUS":-1.2
    },
    "Ohio State":{
-      "SP_PLUS":25.0
+      "SP_PLUS":26.5
    },
    "Oklahoma":{
-      "SP_PLUS":28.0
+      "SP_PLUS":28.6
    },
    "Oklahoma State":{
       "SP_PLUS":14.1
    },
    "Old Dominion":{
-      "SP_PLUS":-15.9
+      "SP_PLUS":-24.6
    },
    "Ole Miss":{
-      "SP_PLUS":14.9
+      "SP_PLUS":15.1
    },
    "Oregon":{
-      "SP_PLUS":24.2
+      "SP_PLUS":19.0
    },
    "Oregon State":{
-      "SP_PLUS":4.5
+      "SP_PLUS":0.6
    },
    "Penn State":{
-      "SP_PLUS":20.5
-   },
-   "Pittsburgh":{
-      "SP_PLUS":8.3
-   },
-   "Purdue":{
-      "SP_PLUS":8.9
-   },
-   "Rice":{
-      "SP_PLUS":-9.4
-   },
-   "Rutgers":{
-      "SP_PLUS":-6.9
-   },
-   "San Diego State":{
-      "SP_PLUS":-3.0
-   },
-   "San José State":{
-      "SP_PLUS":-2.3
-   },
-   "SMU":{
-      "SP_PLUS":2.5
-   },
-   "South Alabama":{
-      "SP_PLUS":-14.5
-   },
-   "South Carolina":{
-      "SP_PLUS":-5.3
-   },
-   "Southern Mississippi":{
-      "SP_PLUS":-9.3
-   },
-   "South Florida":{
-      "SP_PLUS":-9.9
-   },
-   "Stanford":{
-      "SP_PLUS":1.6
-   },
-   "Syracuse":{
-      "SP_PLUS":-7.0
-   },
-   "TCU":{
-      "SP_PLUS":12.1
-   },
-   "Temple":{
-      "SP_PLUS":-15.9
-   },
-   "Tennessee":{
-      "SP_PLUS":6.1
-   },
-   "Texas":{
-      "SP_PLUS":17.6
-   },
-   "Texas A&M":{
-      "SP_PLUS":20.9
-   },
-   "Texas State":{
-      "SP_PLUS":-13.1
-   },
-   "Texas Tech":{
-      "SP_PLUS":1.2
-   },
-   "Toledo":{
-      "SP_PLUS":-2.1
-   },
-   "Troy":{
-      "SP_PLUS":2.0
-   },
-   "Tulane":{
-      "SP_PLUS":1.9
-   },
-   "Tulsa":{
-      "SP_PLUS":5.0
-   },
-   "UAB":{
-      "SP_PLUS":-0.9
-   },
-   "UCF":{
-      "SP_PLUS":10.0
-   },
-   "UCLA":{
-      "SP_PLUS":15.6
-   },
-   "UMass":{
-      "SP_PLUS":-24.9
-   },
-   "UNLV":{
-      "SP_PLUS":-19.5
-   },
-   "USC":{
-      "SP_PLUS":19.6
-   },
-   "Utah":{
-      "SP_PLUS":17.7
-   },
-   "Utah State":{
-      "SP_PLUS":-17.5
-   },
-   "UTEP":{
-      "SP_PLUS":-24.5
-   },
-   "UT San Antonio":{
-      "SP_PLUS":-4.4
-   },
-   "Vanderbilt":{
-      "SP_PLUS":-12.7
-   },
-   "Virginia":{
-      "SP_PLUS":4.9
-   },
-   "Virginia Tech":{
-      "SP_PLUS":11.0
-   },
-   "Wake Forest":{
-      "SP_PLUS":2.5
-   },
-   "Washington":{
       "SP_PLUS":21.2
    },
+   "Pittsburgh":{
+      "SP_PLUS":8.5
+   },
+   "Purdue":{
+      "SP_PLUS":6.1
+   },
+   "Rice":{
+      "SP_PLUS":-10.5
+   },
+   "Rutgers":{
+      "SP_PLUS":-0.7
+   },
+   "San Diego State":{
+      "SP_PLUS":1.8
+   },
+   "San José State":{
+      "SP_PLUS":-0.8
+   },
+   "SMU":{
+      "SP_PLUS":5.4
+   },
+   "South Alabama":{
+      "SP_PLUS":-13.4
+   },
+   "South Carolina":{
+      "SP_PLUS":-1.5
+   },
+   "Southern Mississippi":{
+      "SP_PLUS":-7.0
+   },
+   "South Florida":{
+      "SP_PLUS":-5.8
+   },
+   "Stanford":{
+      "SP_PLUS":3.3
+   },
+   "Syracuse":{
+      "SP_PLUS":-6.0
+   },
+   "TCU":{
+      "SP_PLUS":11.2
+   },
+   "Temple":{
+      "SP_PLUS":-7.4
+   },
+   "Tennessee":{
+      "SP_PLUS":8.3
+   },
+   "Texas":{
+      "SP_PLUS":16.7
+   },
+   "Texas A&M":{
+      "SP_PLUS":21.6
+   },
+   "Texas State":{
+      "SP_PLUS":-13.0
+   },
+   "Texas Tech":{
+      "SP_PLUS":2.3
+   },
+   "Toledo":{
+      "SP_PLUS":2.7
+   },
+   "Troy":{
+      "SP_PLUS":2.8
+   },
+   "Tulane":{
+      "SP_PLUS":5.8
+   },
+   "Tulsa":{
+      "SP_PLUS":2.2
+   },
+   "UAB":{
+      "SP_PLUS":2.5
+   },
+   "UCF":{
+      "SP_PLUS":11.5
+   },
+   "UCLA":{
+      "SP_PLUS":13.3
+   },
+   "UMass":{
+      "SP_PLUS":-26.2
+   },
+   "UNLV":{
+      "SP_PLUS":-15.7
+   },
+   "USC":{
+      "SP_PLUS":19.7
+   },
+   "Utah":{
+      "SP_PLUS":16.7
+   },
+   "Utah State":{
+      "SP_PLUS":-10.8
+   },
+   "UTEP":{
+      "SP_PLUS":-19.7
+   },
+   "UT San Antonio":{
+      "SP_PLUS":-2.2
+   },
+   "Vanderbilt":{
+      "SP_PLUS":-7.8
+   },
+   "Virginia":{
+      "SP_PLUS":5.8
+   },
+   "Virginia Tech":{
+      "SP_PLUS":11.8
+   },
+   "Wake Forest":{
+      "SP_PLUS":1.6
+   },
+   "Washington":{
+      "SP_PLUS":20.2
+   },
    "Washington State":{
-      "SP_PLUS":7.8
+      "SP_PLUS":7.9
    },
    "Western Kentucky":{
-      "SP_PLUS":-8.1
+      "SP_PLUS":-8.3
    },
    "Western Michigan":{
-      "SP_PLUS":-3.1
+      "SP_PLUS":1.8
    },
    "West Virginia":{
-      "SP_PLUS":12.0
+      "SP_PLUS":8.2
    },
    "Wisconsin":{
       "SP_PLUS":22.4
    },
    "Wyoming":{
-      "SP_PLUS":-2.1
+      "SP_PLUS":0.9
    },
    "New Mexico State":{
       "SP_PLUS":-31.5
    },
    "Liberty":{
-      "SP_PLUS":8.3
+      "SP_PLUS":5.8
    }
 }
