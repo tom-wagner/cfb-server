30871743ab953f4cd555dbbe0b48d79583ec835c
Tom Wagner <tomwa@zillowgroup.com>
2024-08-22 13:28:24 -0500

    running on work computer; code still broken

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # cfb-server
+
+### Fixes to get things to run
+1. `pyenv local 3.9.17`
+2. `pip3 install -r requirements.txt`
+3. Dependencies may have conflicts; use `~=` instead of `==`
+4. `export PYTHONPATH=.`
--- external_apis/cf_data.py
+++ external_apis/cf_data.py
@@ -8,17 +8,15 @@ class CFData:
         configuration = cfbd.Configuration()
         configuration.api_key['Authorization'] = 'v5TLyLl9pmeFDqKog4FsFwj2gv8x5Pwat4G6fqJf4SCBEsrbL93Xgcgz2gLXVC0f'
         configuration.api_key_prefix['Authorization'] = 'Bearer'
-        
-        self.api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
-        
 
-        # def get(self, route: str, **params):
-        #     return requests.get(f'{self.base_path}/{route}', params={ **params, 'Authorization': 'Bearer v5TLyLl9pmeFDqKog4FsFwj2gv8x5Pwat4G6fqJf4SCBEsrbL93Xgcgz2gLXVC0f' })
+        self.base_path = BASE_CFD_API
+        self.api_instance = cfbd.GamesApi(cfbd.ApiClient(configuration))
+        self.teams_instance = cfbd.TeamsApi(cfbd.ApiClient(configuration))
 
     def get_schedule(self, year: int, conference: str):
         if conference:
-            return self.api_instance.get_games(year=year, conference=conference)
-        return self.api_instance.get_games(year=year)
+            return self.api_instance.get_games(year=year, conference=conference).json()
+        return self.api_instance.get_games(year=year) # .json()
 
     def get_teams(self):
-        return self.get("teams").json()
+        return self.teams_instance.get_teams()
